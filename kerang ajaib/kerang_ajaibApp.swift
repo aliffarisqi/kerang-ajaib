342ac8a1e3f859e8c2e51dd1e65b68ab34d01bfb
Bayu Alif Farisqi <bayualiev@gmail.com>
2023-03-18 16:33:01 +0700

    Initial Commit

---
+++ kerang ajaib/kerang_ajaibApp.swift
@@ -0,0 +1,17 @@
+//
+//  kerang_ajaibApp.swift
+//  kerang ajaib
+//
+//  Created by Bayu Alif Farisqi on 18/03/23.
+//
+
+import SwiftUI
+
+@main
+struct kerang_ajaibApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
